5dd45b0d83c4bd3510be952f7ef40c50d0d5dd15
Pavithran Arul <pavithranarul7@gmail.com>
2024-03-14 08:55:20 +0530

    Update ex5s.py

--- cd/ex5s.py
+++ cd/ex5s.py
@@ -1,48 +1,46 @@
-import sys
-sys.setrecursionlimit(60)
+# import sys
+# sys.setrecursionlimit(60)
 
-def first(string, visited=None):
-    if visited is None:
-        visited = set()
-    
+def first(string):
+    #print("first({})".format(string))
     first_ = set()
     if string in non_terminals:
-        if string in visited:
-            return first_
-        visited.add(string)
-
         alternatives = productions_dict[string]
 
         for alternative in alternatives:
-            first_2 = first(alternative, visited)
-            first_ = first_ | first_2
+            first_2 = first(alternative)
+            first_ = first_ |first_2
 
     elif string in terminals:
         first_ = {string}
 
-    elif string == '' or string == '@':
+    elif string=='' or string=='@':
         first_ = {'@'}
 
     else:
-        first_2 = first(string[0], visited)
+        first_2 = first(string[0])
         if '@' in first_2:
             i = 1
             while '@' in first_2:
+                #print("inside while")
+
                 first_ = first_ | (first_2 - {'@'})
+                #print('string[i:]=', string[i:])
                 if string[i:] in terminals:
                     first_ = first_ | {string[i:]}
                     break
                 elif string[i:] == '':
                     first_ = first_ | {'@'}
                     break
-                first_2 = first(string[i:], visited)
+                first_2 = first(string[i:])
                 first_ = first_ | first_2 - {'@'}
                 i += 1
         else:
             first_ = first_ | first_2
 
-    return first_
 
+    #print("returning for first({})".format(string),first_)
+    return  first_
 
 
 def follow(nT):
@@ -72,13 +70,11 @@ def follow(nT):
                             follow_ = follow_ | follow_2
     #print("returning for follow({})".format(nT),follow_)
     return follow_
-
 no_of_terminals=int(input("Enter no. of terminals: "))
 terminals = []
 print("Enter the terminals :")
 for _ in range(no_of_terminals):
     terminals.append(input())
-
 no_of_non_terminals=int(input("Enter no. of non terminals: "))
 non_terminals = []
 print("Enter the non terminals :")
@@ -88,21 +84,40 @@ for _ in range(no_of_non_terminals):
 starting_symbol = input("Enter the starting symbol: ")
 
 no_of_productions = int(input("Enter no of productions: "))
+
 productions = []
+
 print("Enter the productions:")
 for _ in range(no_of_productions):
     productions.append(input())
+
+
+#print("terminals", terminals)
+
+#print("non terminals", non_terminals)
+
+#print("productions",productions)
+
 
 productions_dict = {}
 
 for nT in non_terminals:
     productions_dict[nT] = []
 
+
+#print("productions_dict",productions_dict)
+
 for production in productions:
     nonterm_to_prod = production.split("->")
     alternatives = nonterm_to_prod[1].split("/")
     for alternative in alternatives:
         productions_dict[nonterm_to_prod[0]].append(alternative)
+
+#print("productions_dict",productions_dict)
+
+#print("nonterm_to_prod",nonterm_to_prod)
+#print("alternatives",alternatives)
+
 
 FIRST = {}
 FOLLOW = {}
@@ -113,12 +128,19 @@ for non_terminal in non_terminals:
 for non_terminal in non_terminals:
     FOLLOW[non_terminal] = set()
 
+#print("FIRST",FIRST)
+
 for non_terminal in non_terminals:
     FIRST[non_terminal] = FIRST[non_terminal] | first(non_terminal)
-    
+
+#print("FIRST",FIRST)
+
+
 FOLLOW[starting_symbol] = FOLLOW[starting_symbol] | {'$'}
 for non_terminal in non_terminals:
     FOLLOW[non_terminal] = FOLLOW[non_terminal] | follow(non_terminal)
+
+#print("FOLLOW", FOLLOW)
 
 print("{: ^20}{: ^20}{: ^20}".format('Non Terminals','First','Follow'))
 for non_terminal in non_terminals:
