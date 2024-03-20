8b70495a5d0e843d42ced2152e4fc64201d49c47
Pavithran Arul <pavithranarul7@gmail.com>
2024-03-21 00:09:07 +0530

    Update ex5.py

--- cd/ex5s.py
+++ cd/ex5s.py
@@ -1,8 +1,7 @@
-# import sys
-# sys.setrecursionlimit(60)
+import sys
+sys.setrecursionlimit(60)
 
 def first(string):
-    #print("first({})".format(string))
     first_ = set()
     if string in non_terminals:
         alternatives = productions_dict[string]
@@ -22,10 +21,7 @@ def first(string):
         if '@' in first_2:
             i = 1
             while '@' in first_2:
-                #print("inside while")
-
                 first_ = first_ | (first_2 - {'@'})
-                #print('string[i:]=', string[i:])
                 if string[i:] in terminals:
                     first_ = first_ | {string[i:]}
                     break
@@ -38,20 +34,15 @@ def first(string):
         else:
             first_ = first_ | first_2
 
-
-    #print("returning for first({})".format(string),first_)
     return  first_
 
 
 def follow(nT):
-    #print("inside follow({})".format(nT))
     follow_ = set()
-    #print("FOLLOW", FOLLOW)
     prods = productions_dict.items()
     if nT==starting_symbol:
         follow_ = follow_ | {'$'}
     for nt,rhs in prods:
-        #print("nt to rhs", nt,rhs)
         for alt in rhs:
             for char in alt:
                 if char==nT:
@@ -68,7 +59,6 @@ def follow(nT):
                             follow_ = follow_ | follow(nt)
                         else:
                             follow_ = follow_ | follow_2
-    #print("returning for follow({})".format(nT),follow_)
     return follow_
 no_of_terminals=int(input("Enter no. of terminals: "))
 terminals = []
@@ -92,32 +82,17 @@ for _ in range(no_of_productions):
     productions.append(input())
 
 
-#print("terminals", terminals)
-
-#print("non terminals", non_terminals)
-
-#print("productions",productions)
-
-
 productions_dict = {}
 
 for nT in non_terminals:
     productions_dict[nT] = []
 
 
-#print("productions_dict",productions_dict)
-
 for production in productions:
     nonterm_to_prod = production.split("->")
     alternatives = nonterm_to_prod[1].split("/")
     for alternative in alternatives:
-        productions_dict[nonterm_to_prod[0]].append(alternative)
-
-#print("productions_dict",productions_dict)
-
-#print("nonterm_to_prod",nonterm_to_prod)
-#print("alternatives",alternatives)
-
+        productions_dict[nonterm_to_prod[0].strip()].append(alternative.strip())  # Use strip() to remove whitespace
 
 FIRST = {}
 FOLLOW = {}
@@ -128,19 +103,14 @@ for non_terminal in non_terminals:
 for non_terminal in non_terminals:
     FOLLOW[non_terminal] = set()
 
-#print("FIRST",FIRST)
-
 for non_terminal in non_terminals:
     FIRST[non_terminal] = FIRST[non_terminal] | first(non_terminal)
-
-#print("FIRST",FIRST)
 
 
 FOLLOW[starting_symbol] = FOLLOW[starting_symbol] | {'$'}
 for non_terminal in non_terminals:
     FOLLOW[non_terminal] = FOLLOW[non_terminal] | follow(non_terminal)
 
-#print("FOLLOW", FOLLOW)
 
 print("{: ^20}{: ^20}{: ^20}".format('Non Terminals','First','Follow'))
 for non_terminal in non_terminals:
